1e6cdfbccd3d202f5463a87c92c6b417ab18b4aa
Roberto Alsina <roberto.alsina@gmail.com>
2025-04-11 10:22:30 -0300

    Basic CLI description

--- src/main.cr
+++ src/main.cr
@@ -3,12 +3,16 @@ require "./libctags"
 
 # Define the usage pattern for docopt
 HELP = <<-DOCOPT
+readtags: search for one or more tags in a ctags file
+
 Usage:
     readtags [-i][-n] <FILE> <NAME>...
     readtags --version
     readtags --help
 
 Options:
+    -i            Case-insensitive search
+    -n            Show line numbers
     -h --help     Show this screen.
     -v --version  Show version.
 DOCOPT
